96224a4ba778a8b054025e2de2015074a2096619
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-07 17:18:36 -0600

    Methods Flow Control HW1 string_practice
    began entering code
    
    [#104970390]

---
+++ numbers_letters_and_variables/string_practice.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+practice_string = 'this is a string to practice with'
+
+# print required lines for this Homework 1 Methods Flow Control story
+puts practice_string
+puts practice_string.capitalize
