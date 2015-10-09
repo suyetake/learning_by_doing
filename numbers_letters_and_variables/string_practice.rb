a6b06411f2f878b02ace0a4f22b80c304ba8faa4
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-09 16:21:55 -0600

    stuck on two lines of output..
    commented out code that is not yet working
    travis is happy (surprised at that)
    rubocop is happy
    
    [#104970390]

--- numbers_letters_and_variables/string_practice.rb
+++ numbers_letters_and_variables/string_practice.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 
+# assign string
 practice_string = 'this is a string to practice with'
 
 # print required lines for this Homework 1 Methods Flow Control story
 puts practice_string
 puts practice_string.capitalize
+puts practice_string.upcase
+puts practice_string.sub(/string/, "'string'").capitalize
+## puts "The string '" + practice_string + "' has " + /
+##   practice_string.count + " characters."
+puts practice_string.reverse
+partial_string = practice_string.split('this is a string to ')
+puts partial_string
+
+## puts partial_string.slice(0..1)
+## puts partial_string.scan(/(...)/)
+## puts partial_string.split(' with')
+## puts partial_string.split('with')
+## puts partial_string.split(' ')
