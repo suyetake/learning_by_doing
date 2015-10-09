847919c95c95813ab2b077e369e9e396b756f6f1
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-09 00:38:25 -0600

    rewrote code from scratch, working but not passing travis
    rubocop is happy, so is my son as we played the game tonight
    travis is not happy about output, but is not specific.
    
    [#105170392]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -5,111 +5,60 @@
 # Pivotal Story 105170392
 
 # Welcoming statements - must hold off for cucumber tests
+# #puts
 # #puts 'Welcome to MadLibs'
 # #puts
-# #puts 'We are going to make a story with your words.'
+# #puts 'I am going to make a story with your words.'
+# #puts
 # #puts
 
-def field_assignment(field_name)
+def ask_for(this_kind_of_word)
   puts
-  print field_name
+  print "Please give me #{this_kind_of_word} ==> "
   gets.chomp.to_s
 end
 
-madlib_adj_field = []
-madlib_adj_field = ['these', 'will', 'get', 'overwritten']
-madlib_noun_field = []
-madlib_noun_field = ['these', 'will', 'get', 'overwritten']
-madlib_verb_field = []
-madlib_verb_field = ['these', 'will', 'get', 'overwritten']
+field_1 = ask_for('an adjective')
+field_2 = ask_for('another adjective')
+field_3 = ask_for('a noun')
+field_4 = ask_for('another noun')
+field_5 = ask_for('a plural noun')
+field_6 = ask_for('a game')
+field_7 = ask_for('a plural noun')
+field_8 = ask_for('a verb ending in "ing"')
+field_9 = ask_for('a verb ending in "ing"')
+field_10 = ask_for('a plural noun')
+field_11 = ask_for('a verb ending in "ing"')
+field_12 = ask_for('a noun')
+field_13 = ask_for('a plant')
+field_14 = ask_for('a part of the body')
+field_15 = ask_for('a place')
+field_16 = ask_for('a verb ending in "ing"')
+field_17 = ask_for('an adjective')
+field_18 = ask_for('a number')
+field_19 = ask_for('a plural noun')
 
-adj_field_name = 'Please give me an adjective: ==> '
-noun_field_name = 'Please give me a noun: ==> '
-verb_field_name = 'Please give me a verb: ==> '
-
-finished_yet = FALSE
-index = 0
-count = 0
-
-# get all the adjectives needed
-num_of_adj = 3
 puts
-puts "Let's first gather #{num_of_adj} adjectives.."
-
-while !finished_yet
-  madlib_adj_field[index] = field_assignment(adj_field_name)
-  index += 1
-  count += 1
-  finished_yet = (count == num_of_adj)
-end
-finished_yet = FALSE
-index = 0
-count = 0
-
-# get all the nouns needed
-num_of_noun = 12
-puts
-puts "Now let's gather #{num_of_noun} nouns.."
-
-while !finished_yet
-  madlib_noun_field[index] = field_assignment(noun_field_name)
-  index += 1
-  count += 1
-  finished_yet = (count == num_of_noun)
-end
-finished_yet = FALSE
-index = 0
-count = 0
-
-# get all the verbs needed
-num_of_verb = 4
-puts
-puts "Lastly, let's gather #{num_of_verb} verbs.."
-
-while !finished_yet
-  madlib_verb_field[index] = field_assignment(verb_field_name)
-  index += 1
-  count += 1
-  finished_yet = (count == num_of_verb)
-end
-
-# next loops for output can go here
-# #finished_yet = FALSE
-# #index = 0
-# #count = 0
-adj_index = 0
-adj_count = 0
-noun_index = 0
-noun_count = 0
-verb_index = 0
-verb_count = 0
-
 puts
 puts 'Here is the story..'
 puts
+puts
 puts 'MADLIBS VACATIONS'
 puts '-----------------'
-puts "A vacation is when you take a trip to some \
-#{madlib_adj_field[adj_index]} place"
-
-# debugging statements produce as expected:
-puts madlib_adj_field[0]
-puts madlib_adj_field[1]
-puts madlib_adj_field[2]
-puts madlib_adj_field[3]
 puts
-puts madlib_noun_field[0]
-puts madlib_noun_field[1]
-puts madlib_noun_field[2]
-puts madlib_noun_field[3]
-puts
-puts madlib_verb_field[0]
-puts madlib_verb_field[1]
-puts madlib_verb_field[2]
-puts madlib_verb_field[3]
-
-# code models for embedding arrays in string outputs
-puts
-puts 'here is the adj array: ' + madlib_adj_field.to_s
-puts "here is the noun array within a string: #{madlib_noun_field}"
-puts "here is a verb element within a string: #{madlib_verb_field[0]}"
+puts "A vacation is when you take a trip to some #{field_1} place"
+puts "with your #{field_2} family. Usually you go to some place"
+puts "that is near a/an #{field_3} or up on a/an #{field_4}."
+puts "A good vacation place is one where you can ride #{field_5}{"
+puts "or play #{field_6} or go hunting for #{field_7} . I like"
+puts "to spend my time #{field_8} or #{field_9}."
+puts 'When parents go on a vacation, they spend their time eating'
+puts "three #{field_10} a day, and fathers play golf, and mothers"
+puts "sit around #{field_11}. Last summer, my little brother"
+puts "fell in a/an #{field_12} and got poison #{field_13} all"
+puts "over his #{field_14}. My family is going to go to (the)"
+puts "#{field_15}, and I will practice #{field_16}. Parents"
+puts 'need vacations more than kids because parents are always very'
+puts "#{field_17} and because they have to work #{field_18}"
+puts "hours every day all year making enough #{field_19} to pay"
+puts 'for the vacation.'
