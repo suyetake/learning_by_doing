131c1ea3d5f3152807a2c0664940bad42b668ffa
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-09 23:14:39 -0600

    corrections made, all happy now
    a few commented out codelines remain from prior trials
    rubocop is happy, so is travis
    
    [#104970390]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -1,18 +1,22 @@
 #!/usr/bin/env ruby
 
-# assign string
+# assign strings
 practice_string = 'this is a string to practice with'
+ppp_string = (practice_string.slice(20, 9) * 3).chop + '!'
 
 # print required lines for this Homework 1 Methods Flow Control story
 puts practice_string
 puts practice_string.capitalize
 puts practice_string.upcase
 puts practice_string.sub(/string/, "'string'").capitalize
-## puts "The string '" + practice_string + "' has " + /
-##   practice_string.count + " characters."
+puts "The string '#{practice_string}' has #{practice_string.length} characters"
 puts practice_string.reverse
-partial_string = practice_string.split('this is a string to ')
-puts partial_string
+puts ppp_string
+
+## puts (practice_string.slice(20,9)*3).chop + '!'
+
+## partial_string = practice_string.split('this is a string to ')
+## puts partial_string
 
 ## puts partial_string.slice(0..1)
 ## puts partial_string.scan(/(...)/)
