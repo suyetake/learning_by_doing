006687f92103361293f1426cc44b322fba80340d
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-17 23:01:21 -0600

    completed all array output lines
    guard and travis cucumber are happy
    
    [#105170810]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -3,13 +3,19 @@
 # create an array to work with
 # that has integers 1-10
 play_array = (1..10).to_a
+
+# my observations while coding this:
 # puts play_array
 # puts play_array.class
-print play_array
-puts ''
+# print play_array
+# puts ''
 
-# create the requisite output lines in PV#105170810
-puts play_array.join('...') + '...'
+# Learnings:
+
+# surprised when later changes to dup_array affected orig_array
+# dup_array = orig_array
+# so this was the better way to duplicate arrays
+# dup_array = orig_array.take(10)
 
 # working, but tricky to chop the last comma:
 # print 'T-'
@@ -19,5 +25,50 @@ puts play_array.join('...') + '...'
 # working, but better practice to place in string
 # print 'T-' + play_array.reverse.join(', ') + '... BLASTOFF!'
 
-blastoff_string = 'T-' + play_array.reverse.join(', ') + '... BLASTOFF!'
+# calculate stuff needed for the output lines
+blastoff_string = 'T-' + play_array.reverse.join(', ') + '...  BLASTOFF!'
+
+index = 3
+first_four = play_array[0..3].join(', ')
+
+shortened_array = play_array.take(10)
+shortened_array.delete(5)
+shortened_array.delete(6)
+shortened_array.delete(7)
+
+rotated_array = shortened_array.take(7)
+rotated_array.push(5)
+rotated_array.rotate(-1)
+
+prepend = 5
+postpend = 6
+
+# create the requisite output lines in PV#105170810
+puts play_array.join('...') + '...'
+
 puts blastoff_string
+
+puts "The last element is #{play_array.last}"
+puts "The first element is #{play_array.first}"
+puts "The third element is #{play_array[2]}"
+puts "The element with an index of #{index} is #{play_array[index]}"
+puts "The second from last element is #{play_array.fetch(-2)}"
+puts "The first four elements are '#{first_four}'"
+
+print 'If we delete 5, 6 and 7 from the array, '
+puts "we're left with [#{shortened_array.join(',')}]"
+
+print "If we add #{prepend} at the beginning of the array, "
+shortened_array.insert(0, prepend)
+puts "we're left with [#{shortened_array.join(',')}]"
+
+print "If we add #{postpend} at the end of the array, "
+shortened_array.push(postpend)
+puts "we're left with [#{shortened_array.join(',')}]"
+
+print "Only the elements #{play_array.select { |num| num > 8 }} "
+puts 'are > 8.'
+
+print 'If we remove all the elements, then the length '
+play_array.pop(play_array.length)
+puts "of the array is #{play_array.length}"
