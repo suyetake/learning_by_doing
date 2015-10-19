48a3ad396b87dd495d3dd15fa0277a2114c6c0de
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-19 15:24:10 -0600

    program is working, but..
    cucumber and rubocop are now happy, yet..
    the numbers array is still being used as an iterator.
    likely not in the spirit of the assignment.
    
    [#105170908]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -7,22 +7,46 @@
 # replacing Fizz for numbers divisible by 3 only
 # replacing Buzz for numbers divisible by 5 only
 
+# instead of an iterator, use an array 1-100
+numbers = (1..100).to_a
+value = 101
+
+# place output to an array
+output = []
+
+# loop through 100
+# while numbers != nil do
+# while !numbers.index.nil?
+while value > 1
+  value = numbers.pop
+  if value % 3 == 0 && value % 5 == 0
+    output.push('FizzBuzz')
+  elsif value % 3 == 0
+    output.push('Fizz')
+  elsif value % 5 == 0
+    output.push('Buzz')
+  else
+    output.push(value)
+  end
+end
+
+puts output.reverse
 
 ### Accepted fizz_buzz.rb Solution ###
 
 # initialize current_number
-current_number = 0
-
-# loop current_number through 100
-while current_number <= 99
-  current_number += 1
-  if current_number % 3 == 0 && current_number % 5 == 0
-    puts 'FizzBuzz'
-  elsif current_number % 3 == 0
-    puts 'Fizz'
-  elsif current_number % 5 == 0
-    puts 'Buzz'
-  else
-    puts current_number
-  end
-end
+# current_number = 0
+#
+# # loop current_number through 100
+# while current_number <= 99
+#   current_number += 1
+#   if current_number % 3 == 0 && current_number % 5 == 0
+#     puts 'FizzBuzz'
+#   elsif current_number % 3 == 0
+#     puts 'Fizz'
+#   elsif current_number % 5 == 0
+#     puts 'Buzz'
+#   else
+#     puts current_number
+#   end
+# end
