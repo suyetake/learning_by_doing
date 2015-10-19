9db6fdce0102c3c0148d364ad49982934ac1349a
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-19 11:38:06 -0600

    added code from prior accepted story
    this is my starting point to refactor to array usage
    
    [#105170908]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -1,4 +1,28 @@
 #!/usr/bin/env ruby
 
-# Implement Fizz Buzz into an array
+# Implement Fizz Buzz into an array in fizz_buzz_array.rb
 
+# output integers 1 through 100:
+# replacing FizzBuzz for numbers divisible by 3 and 5
+# replacing Fizz for numbers divisible by 3 only
+# replacing Buzz for numbers divisible by 5 only
+
+
+### Accepted fizz_buzz.rb Solution ###
+
+# initialize current_number
+current_number = 0
+
+# loop current_number through 100
+while current_number <= 99
+  current_number += 1
+  if current_number % 3 == 0 && current_number % 5 == 0
+    puts 'FizzBuzz'
+  elsif current_number % 3 == 0
+    puts 'Fizz'
+  elsif current_number % 5 == 0
+    puts 'Buzz'
+  else
+    puts current_number
+  end
+end
