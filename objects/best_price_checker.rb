f9f9af701502569e51bba626ecd4959346e99d97
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-21 14:21:34 -0600

    added code outline in comments and empty defs
    coding underway
    
    [#105427000]

--- objects/best_price_checker.rb
+++ objects/best_price_checker.rb
@@ -1,2 +1,32 @@
 # BestPriceChecker class
 # Homework 2 in Builtin and Custom Objects slide deck
+class BestPriceChecker
+  attr_reader :product_name, :price, :quantity, :brand
+
+  def initialize(product_name, price, quantity, brand)
+
+  end
+
+  def add_product
+
+  end
+  def add_price
+
+  end
+  def add_quantity
+
+  end
+  def add_brand
+
+  end
+
+  # discover ppq (price per quantity) and store value
+  def calc_ppq
+
+  end
+
+  # compare ppq stored for a given product across brands
+  # and return the lowest ppq brand name and quantity
+  def best_price(product_name)
+  end
+end
