1fa8aea2bab8b42985b6b9aa58c32116efdd29ee
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-21 13:49:40 -0600

    final changes
    refined formatting and applying DRY
    Don't Repeat Yourself by using a couple new methods.
    
    [#105426648]

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -2,26 +2,47 @@
 # homework 1 in Builtin and Custom Objects slide deck
 class CashRegister
   def initialize
-    @balance = 0.0
+    @balance = 0.00
+  end
+
+  # rounding method, which assumes 2 decimal places
+  # unless told otherwise by passing a second parameter
+  def round_to(amount, d_places = 2)
+    amount.round(d_places)
+  end
+
+  # to get the output to two (or other) decimal places
+  def to_dollar(amount)
+    round_to(@balance)
+    format('%.2f', amount)
   end
 
   def purchase(purchase)
-    @balance += purchase.to_f
-    @balance.round(2)
+    @balance += purchase
+    to_dollar(@balance)
   end
 
   def total
-    @balance.round(2)
+    to_dollar(@balance)
   end
 
-  def pay_cash(cash)
+  def pay(cash)
+    # since @balance may be in the form of a string
+    # from prior to_dollar calls, ensure the math
+    # occurs in floating point by making cash.to_f
     change = cash.to_f - @balance
-    if change.round(2) < 0.0
-      puts "#{change.abs} more money is needed, please.."
+
+    ## not needed I think ##
+    # change = round_to(change)
+
+    if change < 0.00
+      # puts "#{change.abs} more money is needed, please.."
       @balance = change.abs
+      puts "Your new total is $#{to_dollar(@balance)}"
+
     else
-      @balance = 0.0
-      change
+      @balance = 0.00
+      puts "Your change is $#{to_dollar(change)}"
     end
   end
 end
