f06c6e1230896b1f670ee6475d91b66e7302f6fa
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-21 00:21:58 -0600

    working code..
    [#105426648]

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -1,2 +1,21 @@
 # CashRegister Class
-# homework#1 in Builtin and Custom Objects slide deck
+# homework 1 in Builtin and Custom Objects slide deck
+class CashRegister
+  def initialize
+    @balance = 0.0
+  end
+
+  def purchase(purchase)
+    @balance += purchase.to_f
+  end
+
+  def total
+    @balance
+  end
+
+  def pay_cash(cash)
+    change = cash.to_f - @balance
+    @balance = 0.0
+    change
+  end
+end
