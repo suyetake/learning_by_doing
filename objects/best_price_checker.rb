5712941056a0c60377f462ad94a6e41954d90075
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-26 14:34:33 -0600

    sheer confusion in coding and how to call the code
    saving here so I can toss freely and reference prior work here
    [#105427000]

--- objects/best_price_checker.rb
+++ objects/best_price_checker.rb
@@ -1,39 +1,59 @@
 # BestPriceChecker class
 # Homework 2 in Builtin and Custom Objects slide deck
 class BestPriceChecker
-  # attr_reader :product, :price, :quantity, :unit
 
-  # def product
-  #   @product
-  # end
-
-  def initialize(brand = 'Heinz', store = 'Safeway', product = 'ketchup', price = 2.00, quantity = 12, unit = 'oz')
-    # @product = p
-    # @price = price
-    # @quantity = quantity
-    # @unit = unit
+  def initialize(brand = 'Heinz',
+    store = 'Safeway',
+    product = 'ketchup',
+    price = 2.00,
+    quantity = 12,
     # for now, assume unit is always ounces
-    @hash_of_products = {}
-    @hash_of_products[:brand] = brand
-    @hash_of_products[:store] = store
-    @hash_of_products[:product] = product
-    @hash_of_products[:price] = price
-    @hash_of_products[:quantity] = quantity
-    @hash_of_products[:unit] = unit
-    @hash_of_products[:price_per_quantity] =
-      @hash_of_products[:price] /
-        @hash_of_products[:quantity]
-    @hash_of_products[:price] = to_dollar(@hash_of_products[:price])
-    @hash_of_products[:price_per_quantity] = to_dollar(@hash_of_products[:price_per_quantity])
+    unit = 'oz')
+    @product1_hash = {}
+    @product1_hash[:brand] = brand
+    @product1_hash[:store] = store
+    @product1_hash[:product] = product
+    @product1_hash[:price] = price
+    @product1_hash[:quantity] = quantity
+    @product1_hash[:unit] = unit
+  end
+
+  def apply_price_per_quantity(product_hash)
+    product_hash[:price_per_quantity] =
+      product_hash[:price] /
+        product_hash[:quantity]
+  end
+
+  def best_priced_product
+    @product1_hash[:price] = to_dollar(@product1_hash[:price])
+    @product1_hash[:price_per_quantity] = to_dollar(@product1_hash[:price_per_quantity])
     puts 'Here is the best priced product so far:'
-    puts @hash_of_products
-    print 'it costs $' + @hash_of_products[:price_per_quantity]
+    puts @product1_hash
+    print 'it costs $' + @product1_hash[:price_per_quantity]
     puts ' per ounce'
     puts ''
   end
 
   def to_dollar(amount)
     format('%.2f', amount)
+  end
+
+  def get_another_product_to_compare
+    @product2_hash = BestPriceChecker.new(
+      brand = 'Market Pantry',
+      store = 'Target',
+      product = 'ketchup',
+      price = 3.00,
+      quantity = 16,
+      unit = 'oz')
+    puts 'Product has been added'
+  end
+
+  def compare_products(prior_product, new_product)
+    if new_product[:price_per_quantity] < prior_product[:price_per_quantity]
+      puts 'You found a better priced product!'
+
+    end
   end
 
   def add_product
@@ -63,7 +83,23 @@ class BestPriceChecker
   # and return the lowest ppq brand name and quantity
   def best_price
   end
+
+  apply_price_per_quantity(@product1_hash)
+  best_priced_product
+  get_another_product_to_compare
+  apply_price_per_quantity(@product2_hash)
+  compare_products(@product1_hash, @product2_hash)
+  best_priced_product
 end
-best_price_checker = BestPriceChecker.new
-best_price_checker.add_product
-best_price_checker.calc_price_per_quantity
+product3 = BestPriceChecker.new
+product3.add_product
+product4 = BestPriceChecker.new(
+  brand = 'Market Pantry',
+  store = 'Target',
+  product = 'ketchup',
+  price = 3.00,
+  quantity = 16,
+  unit = 'oz')
+product4.add_product
+BestPriceChecker.compare_products(product3, product4)
+best_priced_product
