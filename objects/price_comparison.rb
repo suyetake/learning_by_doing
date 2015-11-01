4e85118fdd413326fc0f6ccf5fa6a53bcba4d78e
Sue Uyetake <sue.uyetake@gmail.com>
2015-11-01 12:28:15 -0700

    final answer
    all code makes rubocop happy
    commented out code with extra displays and logic not asked for
    
    [#105427000]

--- objects/price_comparison.rb
+++ objects/price_comparison.rb
@@ -1,15 +1,11 @@
 require_relative '../objects/product'
 
 # Class to compare attributes of two products
+# all code makes rubocop happy,
+# commented out extra displays and logic not asked for
 class PriceComparison
   def to_dollar(amount)
     format('%.2f', amount)
-  end
-
-  def display_current
-    print "current product is #{@current_product.name} at "
-    print "$#{to_dollar(@current_product.price)} "
-    puts "for #{@current_product.size} ounces"
   end
 
   def display_next
@@ -19,42 +15,51 @@ class PriceComparison
   end
 
   def display_best
-    print "best product is #{@product.name} at "
-    print "$#{to_dollar(@product.price)} "
-    puts "for #{@product.size} ounces"
+    print "best product is #{@current_product.name} at "
+    print "$#{to_dollar(@current_product.price)} "
+    puts "for #{@current_product.size} ounces"
   end
 
   def better_price(current_product, next_product)
     @current_product = current_product
     @next_product = next_product
+    # @same = false
     current_deal = @current_product.price / @current_product.size
     next_deal = @next_product.price / @next_product.size
+    puts "deals compared: #{to_dollar(current_deal)} #{to_dollar(next_deal)}"
     # display_current
     # display_next
-    @product = @next_product if next_deal < current_deal
-    @product
+    @current_product = @next_product if next_deal < current_deal
+    # @same = true if next_deal == current_deal
     # display_best
+    @current_product
   end
 end
 
-##
-#
-# def initialize
-#   @product = Product.new('Heinz Ketchup', 4.2, 16)
-#   display_best
-# end
-#
-# def initialize(product1, product2)
-#   @p1 = product1
-#   @p2 = product2
-#   product3 = Product.new('foo', 12, 16)
-#   puts "product1 size is #{product1.size}"
-#   puts "product2 size is #{product2.size}"
-#   puts "product3 size is #{product3.size}"
-# end
-#
-# puts "product1 is #{@p1.name} at $#{to_dollar(@p1.price)} /
-# for #{@p1.size} ounces"
-#
-# puts "product2 is #{@p2.name} at $#{to_dollar(@p2.price)} /
-# for #{@p2.size} ounces"
+## Tests
+## second price (Kroger) is better
+heinz = Product.new('Heinz', 5.75, 15)
+store_brand = Product.new('Kroger', 3.00, 12)
+
+comparitor = PriceComparison.new
+
+better1 = comparitor.better_price(heinz, store_brand)
+puts "The better product is the #{better1.name} brand"
+
+## prices are equal, so first product (Heinz) remains
+heinz = Product.new('Heinz', 3.00, 12)
+store_brand = Product.new('Kroger', 3.00, 12)
+
+comparitor = PriceComparison.new
+
+better2 = comparitor.better_price(heinz, store_brand)
+puts "The better product is the #{better2.name} brand"
+
+## first price (Heinz) is better
+heinz = Product.new('Heinz', 3.00, 12)
+store_brand = Product.new('Kroger', 5.75, 15)
+
+comparitor = PriceComparison.new
+
+better3 = comparitor.better_price(heinz, store_brand)
+puts "The better product is the #{better3.name} brand"
