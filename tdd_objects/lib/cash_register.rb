a69e62f8f7c61f3cbd2ef735a4e76c567de98556
Sue Uyetake <sue.uyetake@gmail.com>
2015-11-08 14:57:24 -0700

    coding in process..
    [#105945432]

--- tdd_objects/lib/cash_register.rb
+++ tdd_objects/lib/cash_register.rb
@@ -1,2 +1,21 @@
+# this is the class
 class CashRegister
+  def initialize
+    @purchase = 0.0
+    @total = 0.0
+  end
+
+  def total
+    @total
+  end
+
+  def purchase(amount)
+    @purchase = amount
+    @total = @total + @purchase
+  end
+
+  def pay(amount)
+    @change = @total - amount
+  end
+
 end
--- tdd_objects/spec/lib/cash_register_spec.rb
+++ tdd_objects/spec/lib/cash_register_spec.rb
@@ -1,3 +1,19 @@
 require './lib/cash_register'
 describe CashRegister do
+  it 'should be a CashRegister' do
+    expect(subject).to be_a(CashRegister)
+  end
+
+  it '#total should be zero when created' do
+    expect(subject.total).to eq(0)
+  end
+
+  it '#purchase should add to the total' do
+    expect(subject.purchase(3.78)).to eq(3.78)
+    expect(subject.total).to eq(3.78)
+    expect(subject.pay(5.00)).to eq("Your change is $1.22")
+  end
+
 end
+
+# register.pay(5.00)  # => "Your change is $1.22"
